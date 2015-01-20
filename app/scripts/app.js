1d99f68c20828861f2a1c3bf110721fb873e7269
sirajrkhan <sirajrkhan@live.in>
2015-01-20 13:28:41 +0000

    added contact view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,9 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+    .when('/contact',{
+        templateUrl: 'views/contact.html'
+    })
       .otherwise({
         redirectTo: '/'
       });
